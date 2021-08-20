30aa351dc1723ec03fd692455a065d1ee3065026
KvngAlka <kingsford499@gmail.com>
2021-08-20 15:02:28 +0000

    whatsapp commit

---
+++ src/firebase.js
@@ -0,0 +1,23 @@
+// npm i firebase
+import firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
+};
+
+// ----------------------------------------------------------
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+const provider = new firebase.auth.GoogleAuthProvider();
+
+export { auth, provider };
+export default db;
+// ----------------------------------------------------------
